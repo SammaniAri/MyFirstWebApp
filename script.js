0d8cb057bf345dc78cbb73756b5fd32f339dfe19
Sammani Ariyarathne <101021576+SammaniAri@users.noreply.github.com>
2022-09-28 22:39:57 +0200

    clean

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu */
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  const x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
