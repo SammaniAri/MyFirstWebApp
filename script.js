14daddf06e9720100134d509eae6b1398d778398
Sammani Ariyarathne <101021576+SammaniAri@users.noreply.github.com>
2022-10-02 23:08:25 +0200

    html langfinal touch-ups attribute

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu */
 function myFunction() {
-  const x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  const myLinksContainer = document.getElementById("myLinks");
+  if (myLinksContainer.style.display === "block") {
+    myLinksContainer.style.display = "none";
   } else {
-    x.style.display = "block";
+    myLinksContainer.style.display = "block";
   }
 }
 
+/* Makes the image in present.html flip by mouse-click*/
 function clickImage() {
   let wobbleWobble = document.getElementById("AnimatedImage");
   wobbleWobble.classList.add("presentImage-animated");
@@ -15,12 +16,3 @@ function clickImage() {
 
 let wobbleWobble = document.getElementById("AnimatedImage");
 wobbleWobble.addEventListener("click", clickImage);
-/*Animate the image in present.html on click 
-function clickImage() {
-  let wobbleWobble = document.getElementById("AnimatedImage");
-  box.classList.add("presentImage-animated");
-}
-
-let box = document.getElementById("AnimatedImage");
-box.addEventListener("click", clickImage);
-*/
